4ab7fced2d08526d3b3385e25ac9d9a8d46cc6e6
SatchmoCodes <cjshuman12@gmail.com>
2024-01-28 23:14:10 -0500

    dynamic resizing + uid in scores

--- app/screens/FreePlay.jsx
+++ app/screens/FreePlay.jsx
@@ -61,7 +61,7 @@ let screenWidth = Dimensions.get('window').width * 0.98
 let screenHeight = Dimensions.get('window').height
 let gridItemSize = Math.floor(screenWidth / boardSize)
 console.log(gridItemSize)
-if (screenWidth >= 750) {
+if (screenWidth >= 500) {
   screenWidth = Dimensions.get('window').height * 0.55
   console.log(screenWidth)
   gridItemSize = Math.floor(screenWidth / boardSize)
@@ -154,6 +154,35 @@ const FreePlay = () => {
   //   const unsubscribe = loadInterstitial()
   //   return unsubscribe
   // }, [])
+
+  const [viewportWidth, setViewportWidth] = useState(Dimensions.get('window').width)
+  const [viewportHeight, setViewportHeight] = useState(
+    Dimensions.get('window').height,
+  )
+
+  useEffect(() => {
+    const handleDimensionsChange = ({ window }) => {
+      screenWidth = Dimensions.get('window').width * 0.98
+      screenHeight = Dimensions.get('window').height
+      gridItemSize = Math.floor(screenWidth / boardSize)
+      if (screenWidth >= 500) {
+        screenWidth = Dimensions.get('window').height * 0.55
+        console.log(screenWidth)
+        gridItemSize = Math.floor(screenWidth / boardSize)
+      }
+      setViewportWidth(screenWidth)
+      setViewportHeight(screenHeight)
+      // Handle viewport size changes here
+    }
+
+    // Subscribe to the event when the component mounts
+    Dimensions.addEventListener('change', handleDimensionsChange)
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      Dimensions.removeEventListener('change', handleDimensionsChange)
+    }
+  }, [])
 
   const [colorState, setColorState] = useState(tempSquareArr)
   const [selectedColor, setSelectedColor] = useState(tempSquareArr[0].color)
@@ -399,6 +428,7 @@ const FreePlay = () => {
         size: sizeName,
         boardData: randomArr.toString(),
         createdBy: userName,
+        uid: uid,
         gamemode: 'FreePlay',
         createdAt: serverTimestamp(),
       })
